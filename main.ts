62301d1258e1e3b81c49182a68e7a8185eb82187
kram0ztg <156235879+kram0ztg@users.noreply.github.com>
2024-01-10 12:58:32 -0600

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,18 @@
-
+let mySprite = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . e e . . . . . . . . 
+    . . . . . . e e e . . . . . . . 
+    . . . . . e e e e e . . . . . . 
+    . . . . e e e e e e e . . . . . 
+    . . . . e e 1 e e 1 e . . . . . 
+    . . . e e e f e e f e e . . . . 
+    . . e e e e f e e f e e e . . . 
+    . . e e e e e e e e e e e . . . 
+    . . e e f f f f f f f e e . . . 
+    . . e e e e e e e e e e e . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    . . . . . . . . . . . . . . . . 
+    `, SpriteKind.Player)
